f718ae6d8fc439e7b52aa9202fe9de68a19845f6
Hajar-Zouhair <hj.zouhair@gmail.com>
2023-05-10 18:43:51 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main -  print the last digit of the number stored in the variable n
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
