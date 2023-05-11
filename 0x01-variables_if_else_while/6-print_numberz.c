39e82edcfaa603632014df3016ae975de208a5f9
Hajar-Zouhair <hj.zouhair@gmail.com>
2023-05-10 19:15:03 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main -  print the last digit of the number stored in the variable n
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 56; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
