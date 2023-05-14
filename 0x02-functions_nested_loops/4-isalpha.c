2a85bf7b49f8e97cd6e93697dcea8426e0669132
Hajar-Zouhair <hj.zouhair@gmail.com>
2023-05-13 18:54:45 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * Return: Always 0 (Success)
+ *
+ * These functions check whether c, 
+ * which must have the value of an unsigned char or EOF, 
+ * falls into a certain  character  class according to 
+ * the specified locale.
+ *
+ * @c : character in acii code
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+	{
+		return (1);
+	}
+	return (0);
+}
