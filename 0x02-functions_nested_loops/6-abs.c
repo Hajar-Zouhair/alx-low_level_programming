0f24c1b34d17d241c35195af7e98c3928d3f363d
Hajar-Zouhair <hj.zouhair@gmail.com>
2023-05-13 19:26:25 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - checks for lowercase character.
+ *
+ * Return: Always 0 (Success)
+ *
+ * The abs() function computes the absolute value of the integer j.
+ *
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
