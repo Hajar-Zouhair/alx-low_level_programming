6a9c55d02d7789259fbea94a8a1734903d1533a0
Hajar-Zouhair <hj.zouhair@gmail.com>
2023-05-14 14:01:02 -0700

    14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,12 @@ int main(void)
 	int i;
 	int F0 = 0;
 	int F1 = 1;
-	int s;
+	unsigned s;
 
 	for (i = 0; i <= 49; i++)
 	{
 		s = F1 + F0;
-		printf("%lu", s);
+		printf("%u", s);
 		F0 = F1;
 		F1 = s;
 		if (i == 49)
